438ab18bff8f34f8f1e6f38876169a61b353f2ac
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-07-24 12:22:49 -0700

    Initialize database (#12)

---
+++ packages/database/src/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+// Prevent multiple instances of Prisma Client in development
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+export const prisma = globalThis.__prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  globalThis.__prisma = prisma;
+}
+
+export * from '@prisma/client';
+export default prisma;
