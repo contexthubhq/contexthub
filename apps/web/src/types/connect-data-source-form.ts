24fbc068c43799dbfbdaa6bf79e0807d0db7e6be
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-01 13:57:28 -0700

    I'm too lazy to write a commit message.

--- apps/web/src/types/connect-data-source-form.ts
+++ apps/web/src/types/connect-data-source-form.ts
@@ -1,10 +1,4 @@
-import { z } from 'zod';
-
-export const connectDataSourceFormSchema = z.object({
-  type: z.string().min(1),
-  credentials: z.record(z.string().min(1), z.string().min(1)),
-});
-
-export type ConnectDataSourceFormData = z.infer<
-  typeof connectDataSourceFormSchema
->;
+export type ConnectDataSourceFormData = {
+  type: string;
+  credentials: Record<string, string>;
+};
