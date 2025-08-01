1bf8f7034358e5c4a47541fbf35acb83b2098e19
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-01 15:53:57 -0700

    Add an empty section to data source page (#52)
    * Add an empty section to data source page
    
    * I'm too lazy to write a commit message.

--- apps/web/src/app/data-sources/page.tsx
+++ apps/web/src/app/data-sources/page.tsx
@@ -4,6 +4,9 @@ import { ConnectDataSourceButton } from './connect-data-source-button';
 import { getAvailableDataSources } from './get-available-data-sources';
 import { getDataSourceConnections } from './get-data-source-connections';
 import { connectDataSource } from './connect-data-source';
+import { EmptySection } from '@/components/empty-section';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default async function DataSourcesPage() {
   const availableDataSources = await getAvailableDataSources();
@@ -17,11 +20,33 @@ export default async function DataSourcesPage() {
           action={connectDataSource}
         />
       </PageHeader>
-      <div className="flex flex-col gap-4">
-        {dataSourceConnections.map((connection) => (
-          <div key={connection.id}>{connection.name}</div>
-        ))}
-      </div>
+      {dataSourceConnections.length === 0 ? (
+        <EmptySection
+          title="No data sources"
+          description="Connect a data source to get started."
+        >
+          <div className="flex flex-row gap-2">
+            <ConnectDataSourceButton
+              availableDataSources={availableDataSources}
+              action={connectDataSource}
+            />
+            <Button variant="outline" asChild>
+              <Link
+                href="https://github.com/contexthubhq/contexthub/tree/main/packages/data-sources"
+                target="_blank"
+              >
+                See docs
+              </Link>
+            </Button>
+          </div>
+        </EmptySection>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {dataSourceConnections.map((connection) => (
+            <div key={connection.id}>{connection.name}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
