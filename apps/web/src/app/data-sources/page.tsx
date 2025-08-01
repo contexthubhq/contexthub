0fc8380cac7131087c91c97dee7006713c178786
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-01 16:30:06 -0700

    Data source display

--- apps/web/src/app/data-sources/page.tsx
+++ apps/web/src/app/data-sources/page.tsx
@@ -4,9 +4,7 @@ import { ConnectDataSourceButton } from './connect-data-source-button';
 import { getAvailableDataSources } from './get-available-data-sources';
 import { getDataSourceConnections } from './get-data-source-connections';
 import { connectDataSource } from './connect-data-source';
-import { EmptySection } from '@/components/empty-section';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { DataSourceDisplay } from './data-source-display';
 
 export default async function DataSourcesPage() {
   const availableDataSources = await getAvailableDataSources();
@@ -20,33 +18,11 @@ export default async function DataSourcesPage() {
           action={connectDataSource}
         />
       </PageHeader>
-      {dataSourceConnections.length === 0 ? (
-        <EmptySection
-          title="No data sources"
-          description="Connect a data source to get started."
-        >
-          <div className="flex flex-row gap-2">
-            <ConnectDataSourceButton
-              availableDataSources={availableDataSources}
-              action={connectDataSource}
-            />
-            <Button variant="outline" asChild>
-              <Link
-                href="https://github.com/contexthubhq/contexthub/tree/main/packages/data-sources"
-                target="_blank"
-              >
-                See docs
-              </Link>
-            </Button>
-          </div>
-        </EmptySection>
-      ) : (
-        <div className="flex flex-col gap-4">
-          {dataSourceConnections.map((connection) => (
-            <div key={connection.id}>{connection.name}</div>
-          ))}
-        </div>
-      )}
+      <DataSourceDisplay
+        dataSourceConnections={dataSourceConnections}
+        availableDataSources={availableDataSources}
+        connectDataSource={connectDataSource}
+      />
     </div>
   );
 }
