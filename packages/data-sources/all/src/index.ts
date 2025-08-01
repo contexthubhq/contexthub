06881d6af5511c0b4587110032ae97063744a8e7
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-07-31 18:46:35 -0700

    Remove unimplemented bigquery (#41)

--- packages/data-sources/all/src/index.ts
+++ packages/data-sources/all/src/index.ts
@@ -1,5 +1,4 @@
 // These exports guarantee that the registration is done.
 export { SnowflakeDataSource } from '@contexthub/data-sources-snowflake';
-export { BigQueryDataSource } from '@contexthub/data-sources-bigquery';
 
 export * from '@contexthub/data-sources-common';
