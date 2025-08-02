045e7b635a5a64f591ed8ac1076bc080c33f8424
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-02 14:01:50 -0700

    I'm too lazy to write a commit message.

--- apps/web/src/types/tables-query-result.ts
+++ apps/web/src/types/tables-query-result.ts
@@ -2,14 +2,15 @@ import { tableDefinitionSchema } from '@contexthub/core';
 import { z } from 'zod';
 import { dataSourceTableTreeSchema } from './table-tree';
 
+export const selectedTableSchema = z.object({
+  fullyQualifiedName: z.string(),
+});
 export const tablesQueryResultSchema = z.object({
   tables: z.array(tableDefinitionSchema),
   tableTree: dataSourceTableTreeSchema,
-  selectedTables: z.array(
-    z.object({
-      fullyQualifiedName: z.string(),
-    })
-  ),
+  selectedTables: z.array(selectedTableSchema),
 });
+
+export type SelectedTable = z.infer<typeof selectedTableSchema>;
 
 export type TablesQueryResult = z.infer<typeof tablesQueryResultSchema>;
