372e7374e493afcf5f4f99ccd3b831e15c8e2abb
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-02 11:23:16 -0700

    I'm too lazy to write a commit message.

--- packages/database/prisma/migrations/20250802182304_selected_tables/migration.sql
+++ packages/database/prisma/migrations/20250802182304_selected_tables/migration.sql
@@ -8,5 +8,8 @@ CREATE TABLE "selected_tables" (
     CONSTRAINT "selected_tables_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "selected_tables_connectionId_fullyQualifiedName_key" ON "selected_tables"("connectionId", "fullyQualifiedName");
+
 -- AddForeignKey
 ALTER TABLE "selected_tables" ADD CONSTRAINT "selected_tables_connectionId_fkey" FOREIGN KEY ("connectionId") REFERENCES "data_source_connections"("id") ON DELETE CASCADE ON UPDATE CASCADE;
