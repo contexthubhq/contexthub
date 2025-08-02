922d3789dc854616e3fd1312e0f210a117e781e4
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-02 09:15:56 -0700

    I'm too lazy to write a commit message.

--- apps/web/src/components/layout/page-header.tsx
+++ apps/web/src/components/layout/page-header.tsx
@@ -7,7 +7,7 @@ export function PageHeader({
 }) {
   return (
     <header>
-      <div className="flex w-full items-center justify-between py-4">
+      <div className="flex w-full items-center justify-between py-6">
         <h1 className="text-2xl font-medium">{title}</h1>
         {children}
       </div>
