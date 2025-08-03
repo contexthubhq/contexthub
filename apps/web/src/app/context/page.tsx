3ed86282b3bf861240a967e31f7185e87f9ac228
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-03 09:37:50 -0700

    I'm too lazy to write a commit message.

--- apps/web/src/app/context/page.tsx
+++ apps/web/src/app/context/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { PageHeader } from '@/components/layout/page-header';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { getDataSourceConnections } from '../data-sources/get-data-source-connections';
 import { TableAndColumnContextSection } from './table-and-column-context-section';
 
@@ -9,22 +8,9 @@ export default async function ContextPage() {
   return (
     <div className="space-y-4">
       <PageHeader title="Context" />
-      <Tabs defaultValue="table-column">
-        <TabsList>
-          <TabsTrigger value="table-column">Table & column context</TabsTrigger>
-          <TabsTrigger value="metrics">Metrics</TabsTrigger>
-          <TabsTrigger value="concepts">Concepts</TabsTrigger>
-        </TabsList>
-        <TabsContent value="table-column">
-          <div className="pt-4">
-            <TableAndColumnContextSection
-              dataSourceConnections={dataSourceConnections}
-            />
-          </div>
-        </TabsContent>
-        <TabsContent value="metrics">Metrics</TabsContent>
-        <TabsContent value="concepts">Concepts</TabsContent>
-      </Tabs>
+      <TableAndColumnContextSection
+        dataSourceConnections={dataSourceConnections}
+      />
     </div>
   );
 }
