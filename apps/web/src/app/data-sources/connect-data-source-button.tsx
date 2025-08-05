29480d1bfa05ad0c75771120d9095456fe8d5030
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2025-08-05 13:32:44 -0700

    I'm too lazy to write a commit message.

--- apps/web/src/app/data-sources/connect-data-source-button.tsx
+++ apps/web/src/app/data-sources/connect-data-source-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CheckIcon, ChevronDown, ChevronsUpDownIcon } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
